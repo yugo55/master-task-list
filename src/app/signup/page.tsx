e60fa85f0b0598b3045f2555b0b62c67fca8ecae
yamauchiyugo <yugo.yamauchi@rooter.co.jp>
2025-01-02 11:52:49 +0900

    ログインページ、サインアップページUI完成

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,37 @@
+export default function Signup() {
+  return (
+    <main className="grid place-items-center h-screen">
+      <div>
+        <h1 className="text-center mb-7 font-bold text-2xl">サインアップ</h1>
+
+        <div className="mb-5">
+          <label htmlFor="email" className="mb-1 block">
+            メールアドレス
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="example@gmail.com"
+            className="block p-1 border rounded-lg border-green-300 focus:border-green-500 focus:border-2 outline-none"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label htmlFor="password" className="mb-1 block">
+            パスワード
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="*******"
+            className="block p-1 border rounded-lg border-green-300 focus:border-green-500 focus:border-2 outline-none"
+          />
+        </div>
+
+        <button className="h-[50px] w-[100px] bg-green-300 block mx-auto rounded-lg hover:opacity-70 transition-all">
+          サインアップ
+        </button>
+      </div>
+    </main>
+  );
+}
